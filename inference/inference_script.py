f5c8a623d10a979d008768d7a0b772207ba1101d
keeganasmith <keeganasmith2003@gmail.com>
2025-03-31 14:50:14 -0500

    inference

---
+++ inference/inference_script.py
@@ -0,0 +1,21 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
+import torch
+
+MODEL_PATH = "/ztank/scratch/group/hprc/llm_interactive/llm_base_models/llama-3.1-8B-Instruct/"
+def perform_inference(my_input):
+    model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, device_map="auto")
+    
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+    
+    inputs = tokenizer(my_input, return_tensors="pt")
+    outputs = model.generate(**inputs, max_new_tokens=100, streamer = streamer, pad_token_id=tokenizer.eos_token_id, early_stopping = True)
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    # Remove the input text from the generated output
+    result = generated_text[len(my_input):].strip()
+    return result
+
+if __name__ == "__main__":
+    perform_inference("Hello world in perl")
---
+++ modules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ml GCC/12.2.0
+ml Python/3.10.8
