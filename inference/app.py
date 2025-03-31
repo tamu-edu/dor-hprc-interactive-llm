bc28376590f9edef6a07f655186f84f3cc22a439
Keegan Smith <keeganasmith2003@gmail.com>
2025-03-31 14:56:43 -0500

    perform inference from endpoint

--- inference/app.py
+++ inference/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, jsonify
-
+from inference_script import perform_inference
 app = Flask(__name__)
 
 @app.route('/infer', methods=['POST'])
 def infer():
-    data = request.json
-    prompt = data.get("input", "")
-    
-    response_text = f"Echo: {prompt}"
-    
-    return jsonify({"response": response_text})
+    try:
+        data = request.json
+        prompt = data.get("input", "")
+        max_response_length = int(data.get("length", ""))
+
+        return jsonify({"response": perform_inference(prompt)})
+    except Exception as e:
+        print("failed with exception: ", e)
+        return jsonify({"status": 500})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- inference/inference_script.py
+++ inference/inference_script.py
@@ -2,12 +2,11 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 import torch
 
 MODEL_PATH = "/ztank/scratch/group/hprc/llm_interactive/llm_base_models/llama-3.1-8B-Instruct/"
+model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
 def perform_inference(my_input):
-    model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, device_map="auto")
     
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
     streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
-
     
     inputs = tokenizer(my_input, return_tensors="pt")
     outputs = model.generate(**inputs, max_new_tokens=100, streamer = streamer, pad_token_id=tokenizer.eos_token_id, early_stopping = True)
