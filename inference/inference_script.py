3903f351d92cc934d004a837d8210d7b0056cd1b
keeganasmith <keeganasmith2003@gmail.com>
2025-04-07 14:33:13 -0500

    switch from transformers to vllm

--- inference/inference_script.py
+++ inference/inference_script.py
@@ -1,23 +1,18 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
-import torch
+from vllm import LLM, SamplingParams
 LLAMA_8B_MODEL_PATH = "/ztank/scratch/group/hprc/llm_interactive/llm_base_models/llama-3.1-8B-Instruct/"
-model_dict = {
-        "llama_8B": {
-                "model": AutoModelForCausalLM.from_pretrained(LLAMA_8B_MODEL_PATH,device_map="auto"),
-                "tokenizer": AutoTokenizer.from_pretrained(LLAMA_8B_MODEL_PATH)
-            }
-        }
-def perform_inference(my_input, max_length, model_name):
-    tokenizer = model_dict[model_name]["tokenizer"]
-    model = model_dict[model_name]["model"]
-    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
-    
-    inputs = tokenizer(my_input, return_tensors="pt")
-    outputs = model.generate(**inputs, max_new_tokens=max_length, streamer = streamer, pad_token_id=tokenizer.eos_token_id, early_stopping = True)
-    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    result = generated_text[len(my_input):].strip()
+model_dict = {
+        "llama_8B": LLM(model=LLAMA_8B_MODEL_PATH, tensor_parallel_size=2, max_model_len=2048, max_num_seqs=2)
+}
+def perform_inference(my_input, max_length, model_name):
+    llm = model_dict[model_name]
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=max_length)
+    outputs = llm.generate([my_input], sampling_params)
+    result = ""
+    for output in outputs[0].outputs:
+        result += output.text + "\n";
     return result
 
 if __name__ == "__main__":
-    perform_inference("Hello world in perl", 512, "llama_8B")
+    result = perform_inference("Hello world in perl", 512, "llama_8B")
+    print(result)
