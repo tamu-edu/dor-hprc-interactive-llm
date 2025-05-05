ad1e8cce9ef0b6d026d9882ce6ad4d48e95415b7
Keegan Smith <keeganasmith@gmail.com>
2025-05-05 12:45:44 -0500

    working on ACES

--- inference/app.py
+++ inference/app.py
@@ -1,16 +1,34 @@
 from flask import Flask, request, jsonify
+import subprocess
 from inference_script import perform_inference
+import socket
+import pickle
 app = Flask(__name__)
 @app.route('/infer', methods=['POST'])
 def infer():
     try:
         data = request.json
         prompt = data.get("input", "")
+        print("prompt: ", prompt)
         model = data.get("model", "")
         max_response_length = int(data.get("length", ""))
+        if(max_response_length > 512):
+            return jsonify({"status": 500, "error": "max length too long"})
         return jsonify({"response": perform_inference(prompt, max_response_length, model)})
     except Exception as e:
+        print("got here")
         print("failed with exception: ", e)
-        return jsonify({"status": 500})
+        return jsonify({"status": 500, "error": e})
 if __name__ == '__main__':
+    result = subprocess.run(
+        ["hostname", "-I"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    print(result.stdout.strip().split())
+    ip_address = result.stdout.strip().split()[0]
+    file_name = "/sw/hprc/sw/dor-hprc-venv-manager/codeai/ip.pkl"
+    with open(file_name, "wb") as f:
+        pickle.dump(ip_address, f)
     app.run(host='0.0.0.0', port=5000)
--- inference/inference_script.py
+++ inference/inference_script.py
@@ -1,5 +1,6 @@
 from vllm import LLM, SamplingParams
 import os
+import re
 cluster = os.environ["CLUSTER"]
 MODEL_PATH = None
 if(cluster == "ACES"):
@@ -8,23 +9,33 @@ elif(cluster == "LAUNCH"):
     MODEL_PATH = "/ztank/scratch/group/hprc/torch_tune/llm_base_models/llama-3.1-8B-Instruct/"
 model_dict = {}
 if(cluster == "ACES"):
+    NUM_GPUS = int(os.environ["NUM_GPUS"])
     model_dict = {
-            "llama_8B": LLM(cpu_offload_gb=20, model=MODEL_PATH,dtype="bfloat16",enforce_eager=True, tensor_parallel_size=8,max_model_len=256, max_num_seqs=1, device="xpu")
+            "llama_8B":LLM(model=MODEL_PATH,dtype="bfloat16",enforce_eager=True,tensor_parallel_size=NUM_GPUS,max_model_len=1024, max_num_seqs=1, device="xpu")
     }
 elif(cluster == "LAUNCH"):
     model_dict = {
             "llama_8B": LLM(model=MODEL_PATH, tensor_parallel_size=2,max_model_len=2048, max_num_seqs=2, device="cuda")
     }
+def remove_ansi_sequences(text):
+    ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
+    return ansi_escape.sub('', text)
 
 def perform_inference(my_input, max_length, model_name):
+    my_input = remove_ansi_sequences(my_input)
     llm = model_dict[model_name]
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=max_length)
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95,max_tokens=max_length, min_tokens = 5)
+    print("my input: ", my_input)
+    print("max length: ", max_length)
     outputs = llm.generate([my_input], sampling_params)
+    print(outputs, flush=True)
     result = ""
     for output in outputs[0].outputs:
+        print(output)
         result += output.text + "\n";
+    print("returning result: ", result, flush=True)
     return result
 
 if __name__ == "__main__":
-    result = perform_inference("Hello world in perl", 512, "llama_8B")
+    result = perform_inference("write hello world in perl", 512, "llama_8B")
     print(result)
